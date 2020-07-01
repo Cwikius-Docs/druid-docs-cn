430afb48049435fab1903bedff8f77fa052e3130
liujianhuan <liujianhuan@qianxin.com>
2020-07-01 13:46:35 +0800

    test add link

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,7 @@
 
 var obox = document.createElement("div")
 obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" target="_blank" title="ElasticSearch源码解析与优化实践">' + 
-                    '<img src = "./img/elasticsearch.jpg" width = "200" height = "200" />' +
+                    '<img src = "/img/elasticsearch.jpg" width = "200" height = "200" />' +
                     '</a>';
 obox.style.position = "fixed";
 obox.style.right = "20px";
