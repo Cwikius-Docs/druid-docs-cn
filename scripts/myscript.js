70299539d36c93f0b7c5e25a6362e0c146d84dc9
liujianhuan <liujianhuan@qianxin.com>
2020-07-01 12:40:32 +0800

    test add link

---
+++ scripts/myscript.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var obox = document.createElement("div")
+    obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" title="ElasticSearch源码解析与优化实践"> < ' + 
+                        'img src = "./elasticsearch.jpg" width = "250" height = "250" / >' +
+                     '</a>';
+    obox.style.position = "fixed";
+    obox.style.right = "20px";
+    obox.style.bottom = "100px";
+
+    var sbox = document.getElementsByClassName(".page-inner")[0];
+    sbox.appendChild(obox);
+}
