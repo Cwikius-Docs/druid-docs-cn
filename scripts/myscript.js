f7cf369b9152850b0796cf22538c10531fa3b2ad
liujianhuan <liujianhuan@qianxin.com>
2020-07-01 13:38:13 +0800

    test add link

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,12 +1,11 @@
-window.onload = function() {
-    var obox = document.createElement("div")
-    obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" target="_blank" title="ElasticSearch源码解析与优化实践">' + 
-                        '<img src = "./img/elasticsearch.jpg" width = "200" height = "200" />' +
-                     '</a>';
-    obox.style.position = "fixed";
-    obox.style.right = "20px";
-    obox.style.bottom = "100px";
 
-    var sbox = document.getElementsByTagName("body")[0];
-    sbox.appendChild(obox);
-}
+var obox = document.createElement("div")
+obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" target="_blank" title="ElasticSearch源码解析与优化实践">' + 
+                    '<img src = "./img/elasticsearch.jpg" width = "200" height = "200" />' +
+                    '</a>';
+obox.style.position = "fixed";
+obox.style.right = "20px";
+obox.style.bottom = "100px";
+
+var sbox = document.getElementsByTagName("body")[0];
+sbox.appendChild(obox);
