ac09fb7181688062f8fbff10e66b330b9ce3e5c2
liujianhuan <liujianhuan@qianxin.com>
2020-07-01 16:10:42 +0800

    test add link

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,21 +1,20 @@
 
-var obox = document.createElement("div")
-obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" target="_blank" title="ElasticSearch源码解析与优化实践">' + 
+var obox1 = document.createElement("div")
+obox1.innerHTML = '<a href="https://u.jd.com/Uj4bve" target="_blank" title="ElasticSearch源码解析与优化实践">' + 
                     '<img src = "/img/elasticsearch.jpg" width = "200" height = "200" />' +
                     '</a>';
-obox.style.position = "fixed";
-obox.style.right = "20px";
-obox.style.bottom = "100px";
+obox1.style.position = "fixed";
+obox1.style.right = "20px";
+obox1.style.bottom = "100px";
+
+var obox2 = document.createElement("div")
+obox2.innerHTML = '<a href="https://union-click.jd.com/jdc?e=&p=AyIGZRhYEQYVB1cTXhIyFwNRH1oWABICVh9rUV1KWQorAlBHU0VeBUVOWk1RAk8ECllHGAdFBwtaV1MJBAJQXk8JF0EfGQcWA1EaWBcCFwRRDBsZdXtjM284ckBmWCFyLExxRXMKYz51YW1DNUQ4Y2F2UgtsMnZyZmRVWy18YWFkNhpcYQBIbjZCEkl2TWMNbBJ%2BQWZ%2BPUgvZmFFYB18JHV2YkUCTTBecRN4Cm8ifVFxZC5HO0phYmcmbB5je0t1NlIrEHVrezBvKGULZnU2bThcYVJ%2FLEk5YltlWyNiL0V7cGdBGS4laWBhAH1aUld3eCcfX21UbAAueVJCYh4LZRprFQMTBVQdWhYFETdlG1wlQ3wHVBteFAYaAGUaaxUGGgVRE1odABIOVhJrEgIbN1AYWhcBEARSB1sQChACUStrJQEiN2UbWCUBIkU7SVMQCkcOUE4OFVdCAlcdCR0DFQRRGQsVBRUOV0gLQQYiBVQaXxw%3D" target="_blank" title="阿宽红油面皮">' +
+    '<img src = "/img/ab403d810530e8ff.png" width = "200" height = "200" />' +
+    '</a>';
+obox2.style.position = "fixed";
+obox2.style.right = "20px";
+obox2.style.bottom = "320px";
 
 var sbox = document.getElementsByTagName("body")[0];
-sbox.appendChild(obox);
-
-var scbox1 = document.createElement("script")
-scbox1.type = "text/javascript"
-scbox1.innerHTML = 'var jd_union_pid = "3002597012";var jd_union_euid = "";'
-var scbox2 = document.createElement("script")
-scbox2.type = "text/javascript"
-scbox2.src = "//ads-union.jd.com/static/js/union.js"
-
-sbox.appendChild(scbox1)
-sbox.appendChild(scbox2)
+sbox.appendChild(obox1);
+sbox.appendChild(obox2);
