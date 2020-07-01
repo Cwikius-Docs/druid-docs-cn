5506bf1ac60371986fbf649c800e459a3038bcb3
liujianhuan <liujianhuan@qianxin.com>
2020-07-01 12:52:19 +0800

    test add link

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var obox = document.createElement("div")
-    obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" title="ElasticSearch源码解析与优化实践"> < ' + 
-                        'img src = "./elasticsearch.jpg" width = "250" height = "250" / >' +
+    obox.innerHTML = '<a href="https://u.jd.com/Uj4bve" title="ElasticSearch源码解析与优化实践">' + 
+                        '<img src = "./elasticsearch.jpg" width = "250" height = "250" />' +
                      '</a>';
     obox.style.position = "fixed";
     obox.style.right = "20px";
