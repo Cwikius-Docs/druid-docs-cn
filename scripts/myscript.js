35e3e617f60ca2881bd8fb40d926b42d33a87f54
liujianhuan <liujianhuan@qianxin.com>
2020-07-13 16:10:33 +0800

    add druid book

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -7,5 +7,14 @@ obox1.style.position = "fixed";
 obox1.style.right = "20px";
 obox1.style.bottom = "100px";
 
+var obox2 = document.createElement("div")
+obox2.innerHTML = '<a href="https://union-click.jd.com/jdc?e=&p=AyIGZRprFQESDl0YWxUyVlgNRQQlW1dCFFlQCxxKQgFHREkdSVJKSQVJHFRXFk9FUlpGQUpLCVBaTFhbXQtWVmpSWRtYFQsaBFUba1NGZwAWRg0UYUtDPVoaaFQTTDBsPWUOHjdUK1sUAxAGUxpYEgEiN1Uca15sEzdUK1sRChcAVh1cEAcaAlUrXBULIgJWGlkWABEASRteHQAXA2UraxYyIjdVK1glQHxUARMMRwQRVAdMUkYHQlQGEl8cAREBUxoPQAcWB1BJWiUAEwZREg%3D%3D" target="_blank" title="企业大数据处理：Spark、Druid、Flume与Kafka应用实践">' +
+    '<img src = "/img/59c07554N924c33a7.jpg" width = "200" height = "200" />' +
+    '</a>';
+obox2.style.position = "fixed";
+obox2.style.right = "20px";
+obox2.style.bottom = "310px";
+
 var sbox = document.getElementsByTagName("body")[0];
 sbox.appendChild(obox1);
+sbox.appendChild(obox2);
