2fec1231a7f1a8c556bc44ac54df8dc183ec8803
liujianhuan <liujianhuan@qianxin.com>
2021-01-05 15:21:28 +0800

    add ads

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,29 +1,10 @@
 
 var obox1 = document.createElement("div")
-obox1.innerHTML = '<a href="https://u.jd.com/Uj4bve" target="_blank" title="ElasticSearch源码解析与优化实践">' + 
-                    '<img src = "/img/elasticsearch.jpg" width = "200" height = "200" />' +
-                    '</a>';
+obox1.innerHTML = '<img src = "/img/movie.jpeg" width = "270" height = "380" />';
 obox1.style.position = "fixed";
 obox1.style.right = "20px";
 obox1.style.bottom = "200px";
 
-var obox2 = document.createElement("div")
-obox2.innerHTML = '<a href="https://union-click.jd.com/jdc?e=&p=AyIGZRprFQESDl0YWxUyVlgNRQQlW1dCFFlQCxxKQgFHREkdSVJKSQVJHFRXFk9FUlpGQUpLCVBaTFhbXQtWVmpSWRtYFQsaBFUba1NGZwAWRg0UYUtDPVoaaFQTTDBsPWUOHjdUK1sUAxAGUxpYEgEiN1Uca15sEzdUK1sRChcAVh1cEAcaAlUrXBULIgJWGlkWABEASRteHQAXA2UraxYyIjdVK1glQHxUARMMRwQRVAdMUkYHQlQGEl8cAREBUxoPQAcWB1BJWiUAEwZREg%3D%3D" target="_blank" title="企业大数据处理：Spark、Druid、Flume与Kafka应用实践">' +
-    '<img src = "/img/59c07554N924c33a7.jpg" width = "200" height = "200" />' +
-    '</a>';
-obox2.style.position = "fixed";
-obox2.style.right = "20px";
-obox2.style.bottom = "400px";
-
-var obox3 = document.createElement("div")
-obox3.innerHTML = '<a href="https://union-click.jd.com/jdc?e=&p=AyIGZRprFQETA1YdXhAyVlgNRQQlW1dCFFlQCxxKQgFHREkdSVJKSQVJHFRXFk9FUlpGQUpLCVBaTFhbXQtWVmpSWRtYFAYRAVAea01lT3c0ZQ9GYGwdUlwndltxAVN9W1MOHjdUK1sUAxAGUxpYEgEiN1Uca0NsEgZUGloUBxICVitaJQIWD1AdUxcGEg5XG1wlBRIOZR5YFAARBVYcRxUHGgVQH2slMhE3ZStbJQEiRTsSDkdVG1AHTw9CVxcCBU5ZRldHAwFPDxVXGwdcSAtAByIFVBpfHA%3D%3D" target="_blank" title="大数据架构详解">' +
-    '<img src = "/img/5804fe12N50fda30b.jpg" width = "200" height = "200" />' +
-    '</a>';
-obox3.style.position = "fixed";
-obox3.style.right = "20px";
-obox3.style.bottom = "600px";
 
 var sbox = document.getElementsByTagName("body")[0];
 sbox.appendChild(obox1);
-sbox.appendChild(obox2);
-sbox.appendChild(obox3);
