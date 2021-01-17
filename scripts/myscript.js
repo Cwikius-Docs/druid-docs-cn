33e646c9486382879d54df2406445a48ec2a0a43
liujianhuan <liujianhuan@qianxin.com>
2021-01-17 11:54:23 +0800

    add test link

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -8,6 +8,23 @@ obox1.style.display = "none";
 
 obox1.setAttribute("class", "movie")
 
+var obox2 = document.createElement("div")
+obox2.innerHTML = '<img src = "/img/support.png" width = "200" height = "90" />';
+obox2.style.position = "fixed";
+obox2.style.bottom = "0px";
+obox2.style.left = "0px";
+obox2.style.display = "none";
+
 var sbox = document.getElementsByTagName("body")[0];
 sbox.appendChild(obox1);
+
+if (Math.random() > 0.7) {
+    sbox.appendChild(obox2);
+    setTimeout(function(){
+        obox2.style.display = "block";
+        setTimeout(function(){
+            obox2.style.display = "none";
+        }, 10000)
+    }, 5000)
+}
 
