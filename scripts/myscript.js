373a3f97c23a9a63d4c7cfd9fccf0fb992d75907
liujianhuan <liujianhuan@qianxin.com>
2021-01-20 11:10:52 +0800

    add qrcode

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,9 +1,9 @@
 
 var obox1 = document.createElement("div")
-obox1.innerHTML = '<img src = "/img/movie.jpeg" width = "275" height = "370" />';
+obox1.innerHTML = '<img src = "/img/qrcode.png" width = "275" height = "300" />';
 obox1.style.position = "fixed";
-obox1.style.right = "20px";
-obox1.style.bottom = "0px";
+obox1.style.right = "10px";
+obox1.style.bottom = "10px";
 obox1.style.display = "none";
 
 obox1.setAttribute("class", "movie")
