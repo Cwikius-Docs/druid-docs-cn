761da3aa476ff15733d9821dac141e761a878bba
Jianhuan Liu <hemin179@163.com>
2021-02-01 21:01:29 +0800

    Update myscript.js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -9,7 +9,7 @@ obox1.style.display = "none";
 obox1.setAttribute("class", "movie")
 
 var obox2 = document.createElement("div")
-obox2.innerHTML = '<img src = "/img/support.gif" width = "200" height = "125" />';
+obox2.innerHTML = '<img src = "/img/movie.jpeg" width = "275" height = "370" />';
 obox2.style.position = "fixed";
 obox2.style.bottom = "0px";
 obox2.style.left = "0px";
@@ -22,13 +22,13 @@ sbox.appendChild(obox1);
 var rand = Math.random();
 console.log(rand);
 
-if (rand > 1) {
+if (rand > 0.5) {
     sbox.appendChild(obox2);
     setTimeout(function(){
         obox2.style.display = "block";
         setTimeout(function(){
             obox2.style.display = "none";
-        }, 10000)
+        }, 30000)
     }, 5000)
 }
 
